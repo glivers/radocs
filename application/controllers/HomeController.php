ebf8ec2278a73d780f331d5553253a741ff53d58
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-15 21:30:30 -0800

    Writing the controller class to load the view files

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -18,37 +18,112 @@ use Libraries\CronLibrary\SampleCronController;
 class HomeController extends BaseController {
 
 	/**
-	 *This method loads the homepage 
+	 *This method loads the preface page 
 	 *
 	 *@param null
 	 *@return void
 	 */
-	public function index()
+	public function getIndex()
 	{
-		//define the page title
-		$data['title'] = 'Gliver MVC PHP Framework';
-
 		//get the ending date today
-		View::render('index', $data);
+		View::render('index');
 
-	}
+	}	
 
 	/**
-	*This method initializes a cron job in the library which executes a series of queries in the 
-	*database to keep user information in synch
-	*
-	*@param null
-	*@return void
-	*/
-	public function CronInit(){
-
-		//create an instance of the cronController class
-		$cronControllerObject = new SampleCronController();
-
-		//call the method to lauch the cron job operation
-		$cronControllerObject->init();
+	 *This method loads the preface page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getPreface()
+	{
+		//get the ending date today
+		View::render('preface');
 
 	}
 	
-}
+	/**
+	 *This method loads the installation page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getInstallation()
+	{
+		//get the ending date today
+		View::render('installation');
 
+	}
+	
+
+	/**
+	 *This method loads the  prief tour page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getBrief()
+	{
+		//get the ending date today
+		View::render('brieftour');
+
+	}
+	
+
+	/**
+	 *This method loads the getting started page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getGetstarted()
+	{
+		//get the ending date today
+		View::render('getstarted');
+
+	}
+	
+
+	/**
+	 *This method loads the helpers page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getHelpers()
+	{
+        $data['func_usage_lbl'] = "Fucntion Use";
+		View::render('helpers',$data);
+
+	}
+	
+
+	/**
+	 *This method loads the database page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getDatabase()
+	{
+		//get the ending date today
+		View::render('database');
+
+	}
+	
+
+	/**
+	 *This method loads the caching page 
+	 *
+	 *@param null
+	 *@return void
+	 */
+	public function getCaching()
+	{
+		//get the ending date today
+		View::render('caching');
+
+	}
+		
+}

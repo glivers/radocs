8398dcb8d6ccd980769e6c5266e2ce9bdf43df4f
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-24 11:02:03 +0300

    Changing site title to Gliver - Official Site

--- application/views/header.glade.php
+++ application/views/header.glade.php
@@ -4,7 +4,7 @@
 <link rel="icon" type="image/ico" href="{{Url::assets('img/logo.png')}}">
 
     <meta charset="UTF-8">
-    <title>Gliver - Documentation</title>
+    <title>Gliver - Official Site</title>
     <!-- Custom Fonts -->
     <link href="{{Url::assets('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">

c22c8886d92515084e57a3e428e253c7a67716fd
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-01 11:56:34 +0300

    Adding the Upload helper class to the sidebar menu

--- application/views/sidebar.glade.php
+++ application/views/sidebar.glade.php
@@ -33,6 +33,7 @@
             <div class="list-group">
                 <a href="{{Url::base('home/brief#controllers')}}" class="list-group-item">Controllers</a>
                 <a href="{{Url::base('home/brief#views')}}" class="list-group-item">Views</a>
+                <a href="{{Url::base('home/brief#templating')}}" class="list-group-item">Templating</a>
                 <a href="{{Url::base('home/brief#models')}}" class="list-group-item">Models</a>
             </div>
 
@@ -41,36 +42,14 @@
             <a href="{{Url::base('home/helpers')}}" class="lead">Helpers</a>
             <div class="list-group">
                 <a href="{{Url::base('home/helpers#array')}}" class="list-group-item">Array</a>
-                <a href="{{Url::base('home/helpers#calendar')}}" class="list-group-item">Calendar</a>
-                <a href="{{Url::base('home/helpers#captcha')}}" class="list-group-item">CAPTCHA</a>
-                <a href="{{Url::base('home/helpers#cart')}}#cart" class="list-group-item">Cart</a>
-                <a href="{{Url::base('home/helpers#config')}}" class="list-group-item">Config</a>
                 <a href="{{Url::base('home/helpers#date')}}" class="list-group-item">Date</a>
-                <a href="{{Url::base('home/helpers#directory')}}" class="list-group-item">Directory</a>
-                <a href="{{Url::base('home/helpers#download')}}" class="list-group-item">Download</a>
-                <a href="{{Url::base('home/helpers#email')}}" class="list-group-item">Email</a>
                 <a href="{{Url::base('home/helpers#file')}}" class="list-group-item">File</a>
                 <a href="{{Url::base('home/helpers#form')}}" class="list-group-item">Form</a>
-                <a href="{{Url::base('home/helpers#html')}}" class="list-group-item">HTML</a>
-                <a href="{{Url::base('home/helpers#encryption')}}" class="list-group-item">Encryption</a>
-                <a href="{{Url::base('home/helpers#inflector')}}" class="list-group-item">Inflector</a>
                 <a href="{{Url::base('home/helpers#input')}}" class="list-group-item">Input</a>
-                <a href="{{Url::base('home/helpers#language')}}" class="list-group-item">Language</a>
-                <a href="{{Url::base('home/helpers#migration')}}" class="list-group-item">Migration</a>
-                <a href="{{Url::base('home/helpers#number')}}" class="list-group-item">Number</a>
-                <a href="{{Url::base('home/helpers#pagination')}}" class="list-group-item">Pagination</a>
-                <a href="{{Url::base('home/helpers#sath')}}" class="list-group-item">Path</a>
-                <a href="{{Url::base('home/helpers#security')}}" class="list-group-item">Security</a>
+                <a href="{{Url::base('home/helpers#path')}}" class="list-group-item">Path</a>
+                <a href="{{Url::base('home/helpers#url')}}" class="list-group-item">Url</a>
                 <a href="{{Url::base('home/helpers#session')}}" class="list-group-item">Session</a>
-                <a href="{{Url::base('home/helpers#smiley')}}" class="list-group-item">Smiley</a>
-                <a href="{{Url::base('home/helpers#string')}}" class="list-group-item">String</a>
-                <a href="{{Url::base('home/helpers#template_parser')}}" class="list-group-item">Template Parser</a>
-                <a href="{{Url::base('home/helpers#text')}}" class="list-group-item">Text</a>
-                <a href="{{Url::base('home/helpers#Typography')}}" class="list-group-item">Typography</a>
-                <a href="{{Url::base('home/helpers#unit_testing')}}" class="list-group-item">Unit testing</a>
-                <a href="{{Url::base('home/helpers#validation')}}" class="list-group-item">Validation</a>
-                <a href="{{Url::base('home/helpers#xml')}}" class="list-group-item">XML</a>
-                <a href="{{Url::base('home/helpers#zip')}}" class="list-group-item">ZIP Encoding</a>
+                <a href="{{Url::base('home/helpers#upload')}}" class="list-group-item">Upload</a>
             </div>
 
 
@@ -78,12 +57,9 @@
             <div class="list-group">
                 <a href="{{Url::base('home/database#basic_usage')}}" class="list-group-item">Basic Usage</a>
                 <a href="{{Url::base('home/database#query_builder')}}" class="list-group-item">Query Builder</a>
-                <a href="{{Url::base('home/database#eloquent')}}" class="list-group-item">Eloquent ORM</a>
+                <a href="{{Url::base('home/database#eloquent')}}" class="list-group-item">Intuitive ORM</a>
                 <a href="{{Url::base('home/database#schema_builder')}}" class="list-group-item">Schema Builder</a>
                 <a href="{{Url::base('home/database#migration_seeding')}}" class="list-group-item">Migration/Seeding</a>
-                <a href="{{Url::base('home/database#sql')}}" class="list-group-item">SQL</a>
-                <a href="{{Url::base('home/database#nosql')}}" class="list-group-item">NoSQL-MongoDB</a>
-                <a href="{{Url::base('home/database#postgre')}}" class="list-group-item">PostgreSQl</a>
                 <a href="{{Url::base('home/database#errors')}}" class="list-group-item">Errors/Logging</a>
             </div>
 

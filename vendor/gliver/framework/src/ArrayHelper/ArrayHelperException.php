06e6f4c3fc22d991abf9e5791001d8043725ae3a
Geoffrey Okongo <georizhq@gmail.com>
2019-10-31 11:38:17 -0200

    Installing dependencies with the framework documentation

---
+++ vendor/gliver/framework/src/ArrayHelper/ArrayHelperException.php
@@ -0,0 +1,17 @@
+<?php namespace Gliver\ArrayHelper;
+
+/**
+ *This class handles all exceptions thrown by Template Class
+ *
+ * @author Geoffrey Okongo <geoffrey@gliver.org>
+ * @copyright 2015 - 2030 Geoffrey Okongo
+ * @category Gliver
+ * @package Gliver\Templates\TemplateException
+ * @link https://github.com/gliverphp/gliver
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @version 2.0.1
+ */
+
+use Gliver\Exceptions\HelperException;
+
+class ArrayHelperException extends HelperException {}
---
+++ vendor/gliver/framework/src/Upload/UploadResponseClass.php
@@ -0,0 +1,15 @@
+<?php namespace Gliver\Upload;
+
+/**
+ *This class is the response object for all successful upload attempts
+ *
+ *@author Geoffrey Okongo <geoffrey@gliver.org>
+ *@copyright 2015 - 2030 Geoffrey Okongo
+ *@category Gliver
+ *@package Gliver\Upload
+ *@link https://github.com/gliverphp/framework
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 2.0.1
+ */
+
+class UploadResponseClass {}
